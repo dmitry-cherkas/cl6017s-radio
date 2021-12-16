923ca1c5fab2c9f36f1fb660b7cbf568204faf36
Dmitry Cherkas <cherkas.da@gmail.com>
2021-12-16 23:26:39 +0300

    radio with encoder

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,23 @@
 
 #include "CL6017S.h"
 #include "SSD1306Wire.h"
-#include "AiEsp32RotaryEncoder.h"
+#include <EncButton.h>
 
 #include "images.h"
 
-#define ROTARY_ENCODER_A_PIN 18
-#define ROTARY_ENCODER_B_PIN 19
-#define ROTARY_ENCODER_BUTTON_PIN 5
+#define ROTARY_ENCODER_A_PIN 2
+#define ROTARY_ENCODER_B_PIN 4
+#define ROTARY_ENCODER_BUTTON_PIN 15
 #define ROTARY_ENCODER_STEPS 4
 
-AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
+EncButton<EB_TICK, ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN> enc; // энкодер с кнопкой <A, B, KEY>
 
 #define OLED_ADDR 0x3c
 #define OLED_SDA 5
 #define OLED_SCL 4
 
 CL6017S radio;
-SSD1306Wire display(OLED_ADDR, OLED_SDA, OLED_SCL);
+//SSD1306Wire display(OLED_ADDR, OLED_SDA, OLED_SCL);
 
 /**
  * TODO:
@@ -38,20 +38,20 @@ void setup()
   Serial.begin(115200);
 
   Serial.println("CL6017S Demo");
-
+  /*
   display.init();
   // draw welcome logo
   display.drawXbm(34, 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
   display.display();
   delay(3000); // wait for 3 seconds
-
+*/
   //radio.debugEnable();
   radio.init();
   radio.enable(true);
   radio.seekUp();
   radio.setVolume(14);
 }
-
+/*
 char buff[25];
 void updateScreen()
 {
@@ -70,8 +70,47 @@ void updateScreen()
   display.drawString(0, 26, buff);
   display.display();
 }
+*/
 
 void loop()
 {
-  updateScreen();
+  enc.tick();
+  //updateScreen();
+
+  uint16_t freq = radio.getFrequency();
+
+  if (enc.left() && freq > 8750)
+  {
+    freq -= 10;
+    radio.setFrequency(freq);
+    Serial.print("Frequency: ");
+    Serial.println(freq);
+  }
+
+  if (enc.right() && freq < 10800)
+  {
+    freq += 10;
+    radio.setFrequency(freq);
+    Serial.print("Frequency: ");
+    Serial.println(freq);
+  }
+
+  if (enc.leftH())
+  {
+    Serial.println("Seek down");
+    radio.seekDown();
+  }
+
+  if (enc.rightH())
+  {
+    Serial.println("Seek up");
+    radio.seekUp();
+  }
+
+  // =============== КНОПКА ===============
+  if (enc.click())
+  {
+    Serial.print("Frequency: ");
+    Serial.println(radio.getFrequency());
+  }
 }
